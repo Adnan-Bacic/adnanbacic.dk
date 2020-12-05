1836ddae0a022375514227e631addd83c7f6eae0
Adnan-Bacic <adnan-bacic@hotmail.com>
2020-12-05 18:25:07 +0100

    updated about page ordering of skills. added some skills.

--- src/about.php
+++ src/about.php
@@ -35,14 +35,21 @@
 		</main>
 		</section>
 		<section class="container">
+			<h2 class="text-center" data-aos="fade-up">Skills</h2>
+
+			<hr class="mb-5 mt-5" data-aos="fade-up"></hr>
+
+			<h2 class="mb-5" data-aos="fade-up">Programming languages</h2>
+
 			<div class="row">
 				<div class="media mb-5 col-xl-12 col-sm-12 mx-auto" data-aos="fade-up">
-					<i class="fas fa-code customFA align-self-start" title="Basic web technologies"></i>
+					<a href="javascript.php"><i class="fab fa-js-square customFA align-self-start" title="Javascript"></i></a>
 					<div class="media-body ml-3">
-						<h2 class="mt-0">Basic web technologies</h2>
-						<p>I have experience coding websites myself from scratch.
+						<h2 class="mt-0">Javascript ES6</h2>
+						<p>Javascript is a client-side programming language. It can be used to create interactive websites. Javascript goes togehter with HTML and CSS to define the basis of web technologies.
 						<br>
-						Some of my skills include:</p>
+						In Javascript i have experience manipulating the behavior of sites. I have experience working with the newest versions of Javascript, also called ES6.</p>
+						<h3>Basic web technologies</h3>
 						<div class="media mt-3" data-aos="fade-up">
 							<i class="fab fa-html5 mr-5 customFAsmaller" title="HTML5"></i>
 								<div class="media-body">
@@ -81,25 +88,13 @@
 					</div>
 				</div>
 			</div>
-
-			<div class="row">
-				<div class="media mb-5 col-xl-12 col-sm-12 mx-auto" data-aos="fade-up">
-					<a href="javascript.php"><i class="fab fa-js-square customFA align-self-start" title="Javascript"></i></a>
-					<div class="media-body ml-3">
-						<h2 class="mt-0">Javascript ES6</h2>
-						<p>Javascript is a client-side programming language. It can be used to create interactive websites. Javascript goes togehter with HTML and CSS to define the basis of web technologies.
-						<br>
-						In Javascript i have experience manipulating the behavior of sites. I have experience working with the newest versions of Javascript, also called ES6.</p>
-					</div>
-				</div>
-			</div>
 			
 			<div class="row">
 				<div class="media mb-5 col-xl-12 col-sm-12 mx-auto" data-aos="fade-up">
 					<a href="vue.php"><i class="fab fa-vuejs customFA align-self-start" title="Vue"></i></a>
 					<div class="media-body ml-3">
 						<h2 class="mt-0">Vue</h2>
-						<p>Vue is a framework for creating fast single-page-applications.</p>
+						<p>Vue is a Javascript framework for creating fast single-page-applications.</p>
 						<p class="mb-0">In Vue i have experience creating interactive websites.</p>
 						<div class="media mt-3" data-aos="fade-up">
 						<i class="fas fa-database mr-5 customFAsmaller" title="Firebase"></i>
@@ -119,7 +114,7 @@
 					<a href="react.php"><i class="fab fa-react customFA align-self-start" title="React"></i></a>
 					<div class="media-body ml-3">
 						<h2 class="mt-0">React</h2>
-						<p>React is a framework for creating fast single-page-applications.</p>
+						<p>React is a Javascript framework for creating fast single-page-applications.</p>
 						<p class="mb-0">In React i have experience creating interactive websites.</p>
 						<div class="media mt-3" data-aos="fade-up">
 						<i class="fas fa-database mr-5 customFAsmaller" title="Firebase"></i>
@@ -136,7 +131,7 @@
 
 			<div class="row">
 				<div class="media mb-5 col-xl-12 col-sm-12 mx-auto" data-aos="fade-up">
-					<a href="#"><i class="fab fa-react customFA align-self-start" title="React"></i></a>
+					<i class="fab fa-react customFA align-self-start" title="React"></i>
 					<div class="media-body ml-3">
 						<h2 class="mt-0">React Native</h2>
 						<p>React Native is a framework for creating native mobile applications for Android and IOS.</p>
@@ -148,6 +143,16 @@
 									<p>Firebase is a database system developed by Google.
 									<br>
 									I have experience combining React Native with Firebase to create interactive applications.</p>
+								</div>
+						</div>
+						<div class="media mt-3" data-aos="fade-up">
+						<!--no expo cli icon, so using an alternative icon-->
+							<i class="fas fa-terminal mr-4 customFAsmaller" title="Expo"></i>
+								<div class="media-body">
+									<h3 class="mt-0">Expo CLI</h3>
+									<p>Expo CLI is an alternative to the React Native CLI for creating and developing React Native apps.
+									<br>
+									I have experience working the Expo CLi for creating React Native apps.</p>
 								</div>
 						</div>
 					</div>
@@ -182,12 +187,13 @@
 							<h2 class="mt-0">Wordpress</h2>
 							<p>Wordpress is a Content Management System(CMS). It is a way to create websites withoutr having to code everything yourself and just get the files done.</p>
 							<p class="mb-0">I have experience with Wordpress, both for creating normal websites as well as webshops with the worlds most popular webshop plugin - Woocommerce.</p>
-						<div class="media mt-3" data-aos="fade-up">
-							<i class="fab fa-elementor mr-5 customFAsmaller" title="Elementor"></i>
+							<div class="media mt-3" data-aos="fade-up">
+							<i class="fab fa-yoast mr-4 customFAsmaller" title="Yoast SEO"></i>
 								<div class="media-body">
-									<h3 class="mt-0">Elementor</h3>
-									<p>Elementor is the largest page builder in Wordpress. With Elementor its easy to quickly create a website just with a drag-n-drop page builder.<br>
-									I have some experience working with Elementor.</p>
+									<h3 class="mt-0">Yoast SEO</h3>
+									<p>Yoast SEO is one of the most popular plugins for adding SEO to websites.
+									<br>
+									I have experience working with Yoast to optimize SEO.</p>
 								</div>
 						</div>
 						<div class="media mt-3" data-aos="fade-up">
@@ -199,21 +205,51 @@
 									I have experience working with WooCommerce to manage a webshop.</p>
 								</div>
 						</div>
+						<div class="media mt-3" data-aos="fade-up">
+							<i class="fab fa-elementor mr-5 customFAsmaller" title="Elementor"></i>
+								<div class="media-body">
+									<h3 class="mt-0">Elementor</h3>
+									<p>Elementor is the largest page builder in Wordpress. With Elementor its easy to quickly create a website just with a drag-n-drop page builder.<br>
+									I have some experience working with Elementor.</p>
+								</div>
+						</div>
 					</div>
 				</div>
 			</div>
-			
 			
 			<div class="row">
 				<div class="media mb-5 col-xl-12 col-sm-12 mx-auto" data-aos="fade-up">
 					<a href="angular.php"><i class="fab fa-angular customFA align-self-start" title="Angular"></i></a>
 					<div class="media-body ml-3">
 						<h2 class="mt-0">Angular</h2>
-						<p>Angular is a framework for creating fast single-page websites.</p>
+						<p>Angular is a Javascript framework for creating fast single-page websites.</p>
 						<p class="mb-0">In Angular i have experience creating simple websites.</p>
 					</div>
 				</div>
 			</div>
+
+			<h2 class="mb-5" data-aos="fade-up">Other</h2>
+
+			<div class="row">
+				<div class="media mb-5 col-xl-12 col-sm-12 mx-auto" data-aos="fade-up">
+					<i class="fab fa-git-alt customFA align-self-start" title="Git"></i>
+					<div class="media-body ml-3">
+						<h2 class="mt-0">Git</h2>
+						<p>Git is a version control system widely used for development</p>
+						<p class="mb-0">I have experience using Git for version control of my development.</p>
+						<div class="media mt-3" data-aos="fade-up">
+						<i class="fab fa-github mr-5 customFAsmaller" title="Github"></i>
+								<div class="media-body">
+									<h3 class="mt-0">Github</h3>
+									<p>Github is an online code sharing platform which utalizes Git.
+									<br>
+									I have experience using Github in combination with Git to create branches, pull requests etc.</p>
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
 						
 		</div>
 	</section>
